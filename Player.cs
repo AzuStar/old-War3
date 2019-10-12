bd57442ca98569cbebf74350999297b2b94bf0ad
Azureus <azureus0@icloud.com>
2019-10-12 16:31:41 +0100

    Initial Commit

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static War3Api.Common;
+
+namespace War3.NoxRaven
+{
+    /// <summary>
+    /// Class for player data, might add some neat stuff here later on.
+    /// Use class for real players, or extend to your needs for computer players.
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// Throw-away value for <see cref="PlayerRef"/>.
+        /// </summary>
+        public int Id;
+        /// <summary>
+        /// Real reference to player.
+        /// </summary>
+        public player PlayerRef;
+        public Player(int id)
+        {
+            Id = id;
+            PlayerRef = Player(id);
+        }
+    }
+}
